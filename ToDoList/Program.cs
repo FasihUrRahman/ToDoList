1f8347d2b7d345c243a394b14f4ed4b672554980
Fasih Ur Rahman <fasihurrahman@Fasihs-MacBook-Pro.local>
2023-05-13 04:26:44 +0500

    Update 13-05-2023

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,3 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//msg = welcom
+//msg = Options 1-input value 2-delete value 3-exit
+//Varibales
+int optionSelection;
+string? userInput1 = null, userInput2 = null ;
+bool isCountinue = true;
+//Code
+Console.WriteLine("Welcome To Our Program");
 
+while (isCountinue)
+{
+    Console.WriteLine("Selecte Option\n1 - Input Value\n2 - Display Value\n3 - Delete Value\n4 - Exit");
+    Console.Write(">>");
+    optionSelection = Convert.ToInt32(Console.ReadLine());
+    switch (optionSelection)
+    {
+        case 1:
+            if(userInput1 == null)
+            {
+                Console.Write("Enter Your Message: ");
+                userInput1 = Console.ReadLine();
+                Console.WriteLine("Input is Saved");
+            }
+            else if (userInput2 == null)
+            {
+                Console.Write("Enter Your Message: ");
+                userInput2 = Console.ReadLine();
+                Console.WriteLine("Input is Saved");
+            }
+            else
+            {
+                Console.WriteLine("There's already a data in list. Please Delete First");
+            }
+            Console.Write("Press Any Key To Continue...");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case 2:
+            Console.WriteLine(userInput1);
+            Console.WriteLine(userInput2);
+            Console.WriteLine("Press Any Key To Continue...");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case 3:
+            userInput1 = null;
+            userInput2 = null;
+            Console.WriteLine("Your Data is Removed");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        case 4:
+            isCountinue = false;
+            break;
+        default:
+            Console.WriteLine("Khotti dya sida jaga jaa");
+            break;
+    }
+
+}
+Console.WriteLine("Bye Bye...");
+
+
+Console.ReadLine();
+
+
+
+
+/*if (optionSelection == 1)
+{
+    Console.WriteLine("In Input Value");
+}
+else if(optionSelection == 2)
+{
+    Console.WriteLine("In Delete Value");
+}
+else if(optionSelection == 3)
+{
+    Console.WriteLine("In Exit");
+}
+else
+{
+    Console.WriteLine("Khotti dya sida jaga jaa");
+}*/
---
+++ ToDoListWithArray/Program.cs
@@ -0,0 +1,127 @@
+﻿//ToDoList With Array
+
+//Variables
+int optionSelector, i = 0, deleteSelector, a=0, arryLength; 
+string? inputValue = null, isContinue = "true";
+
+Console.Write("How Many Tasks You Have Today: ");
+arryLength = Convert.ToInt32(Console.ReadLine());
+string[] inputsByUser = new string[arryLength];
+
+
+
+//CodeStart
+//Loop For Restart Code
+while (isContinue == "true")
+{
+    Console.WriteLine("\tTodo List");   //Title Message
+    Console.WriteLine("Options\n1 - Input Value\n2 - Output Value\n3 - Clear Value\n4 - Exit"); //User Selections
+    Console.Write(">>");
+    optionSelector = Convert.ToInt32(Console.ReadLine());   //Input Selctions
+    switch (optionSelector)
+    {
+        case 1:
+            //Getting Input
+            Console.Write("Enter Your Message: ");
+            inputValue = Console.ReadLine();
+            //Conditions To Check Array is Full Or Not
+            if (inputValue == "")
+            {
+                Console.WriteLine("Please Input Something....");
+            }
+            else if ((i + 1) > arryLength)
+            {
+                Console.WriteLine("Please Delete Message First");
+            }
+            else if (inputsByUser[i] == null)
+            {
+                inputsByUser[i] = inputValue;
+                i++;
+                Console.WriteLine("Query Has Been Added");
+            }
+            Console.Write("Hit Any Key To Continue...");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+            //End Getting Input
+        case 2:
+            //OutPut Value
+            
+            a = 0;  //Variable To Check Input Values 
+            if (inputsByUser[a] == null)    //Check if Array is Null
+            {
+                Console.WriteLine("Please Input A Value");
+            }
+            else
+            {
+                for (int j = 0; j < inputsByUser.Length; j++)    //Loop For Check Input Value
+                {
+                    if (inputsByUser[j] != null)    //Condition To Check Values
+                    {
+                        a++;
+                    }
+                }
+                for (int k = 0; k < a; k++) //Loop For Those Values Which are Getting From User
+                {
+                    Console.WriteLine($"Your Message is \"{inputsByUser[k]}\"");    //Queries To Display
+                }
+            }
+            Console.Write("Hit Any Key To Continue...");
+            Console.ReadLine();
+            Console.Clear();
+            break;
+            //End OutPut Value
+        case 3:
+            //Clear Value
+            a = 0;
+            if (inputsByUser[a] == null)    //Check if Array is Null
+            {
+                Console.WriteLine("Please Input A Value Before Delete AnyThing");
+            }
+            else //If Array is Not Null
+            {
+                //Loop To Check input Values
+                for (int j = 0; j < inputsByUser.Length; j++)
+                {
+                    if (inputsByUser[j] != null)
+                    {
+                        a++;
+                    }
+                }
+                Console.WriteLine("Select The Message You wanna Delete");
+                //Loop To Print out Values
+                for (int j = 0; j < a; j++)
+                {
+                    Console.WriteLine($"{j + 1} - \"{inputsByUser[j]}\"");
+                }
+                Console.Write(">>");
+                deleteSelector = Convert.ToInt32(Console.ReadLine());   //Get Value For input
+                //Condition For Check The valid Input
+                if (deleteSelector <= inputsByUser.Length)
+                {
+                    inputsByUser[deleteSelector - 1] = null;
+                    //Loop For Delete and shuffal Values
+                    for (int j = deleteSelector - 1; j < a; j++)
+                    {
+                        inputsByUser[j] = inputsByUser[j + 1];
+                    }
+                    i -= 1;
+                    Console.WriteLine("Your Query Has Been Deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Please Select Right Option");
+                }
+            }
+            Console.Write("Hit Any Key To Continue...");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+            //End Clear Value
+        case 4:
+            //Exit
+            isContinue = "false";   //False While Condiotion
+            break;
+            //End Exit
+    }
+}
